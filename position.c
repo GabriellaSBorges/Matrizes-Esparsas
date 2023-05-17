7d913c3a8f823e0b0898046fa35a0331fc6ab068
GabriellaSBorges <77707319+GabriellaSBorges@users.noreply.github.com>
2023-05-17 16:53:29 -0300

    Add files via upload

---
+++ position.c
@@ -0,0 +1,38 @@
+#include "../headers/position.h"
+
+struct Position{
+    int line; 
+    int column; 
+};
+
+Position_pt position_construct(Position_pt p, int l, int c){
+
+    p = (Position_pt) malloc( sizeof(Position) );
+
+    p->line = l;
+    p->column = c;
+
+    return p; 
+}
+
+void position_destroy(Position_pt p){
+    free(p);
+}
+
+int position_return_line(Position_pt p){
+    return p->line;
+}
+
+int position_return_column(Position_pt p){
+    return p->column;
+}
+
+Position *read_binary_position(FILE *arq){
+
+    Position *p = (Position*) malloc( sizeof(Position) );
+
+    fread( &p->line, sizeof(int), 1, arq);
+    fread( &p->column, sizeof(int), 1, arq);
+
+    return p;
+}
